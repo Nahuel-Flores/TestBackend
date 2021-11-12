2626af9ddfbeb4da3bb25b728137eb2122f2e1f4
nahuelflores <nflores@mobydigital.com>
2021-11-12 11:29:06 -0300

    Creacion de los controllers.

---
+++ src/main/java/com/mobydigital/test/controllers/TecnologiaController.java
@@ -0,0 +1,12 @@
+package com.mobydigital.test.controllers;
+
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log
+@RequestMapping(value = "/api/tecnologia")
+@RestController
+public class TecnologiaController {
+
+}
