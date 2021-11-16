90900cff98a021ef123fca8b2a11a6c78c0070d7
nahuelflores <nflores@mobydigital.com>
2021-11-16 15:25:12 -0300

    Creacion del CandidatoControllerTest y tests de crear, actualizar y listar.

---
+++ src/test/java/com/mobydigital/test/controllers/CandidatoControllerTest.java
@@ -0,0 +1,71 @@
+package com.mobydigital.test.controllers;
+
+import com.mobydigital.test.services.CandidatoService;
+import com.mobydigital.test.services.CandidatoXTecnologiaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static com.mobydigital.test.testutils.TestEntityFactory.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CandidatoController.class)
+public class CandidatoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    CandidatoService candidatoService;
+
+    @MockBean
+    CandidatoXTecnologiaService candidatoXTecnologiaService;
+
+    @InjectMocks
+    CandidatoController candidatoController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @WithMockUser
+    void crearCandidatoTest() throws Exception {
+        when(candidatoService.guardar(getCandidatoDtoSinID())).thenReturn(getCandidatoDto());
+        mockMvc.perform(post("/api/candidato/crear",getCandidatoDtoJSON())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getCandidatoDtoJSON()))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithMockUser
+    void actualizarCandidatoTest() throws Exception {
+        when(candidatoService.modificar(getCandidatoDto())).thenReturn(getCandidatoDto());
+        mockMvc.perform(put("/api/candidato/actualizar",getCandidatoDtoJSON())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getCandidatoDtoJSON()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void listarTodosTest() throws Exception {
+        when(candidatoService.listarTodos()).thenReturn(getListCandidatoDto());
+                mockMvc.perform(get("/api/candidato/listartodos"))
+                        .andExpect(status().isOk())
+                        .equals(getListCandidatoDtoJSON());
+
+    }
+
+
+}
